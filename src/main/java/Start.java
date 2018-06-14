5bb7fbc78d31709b04b681c59b635a8f778dd20e
Tang-L <874567455@qq.com>
2018-06-14 16:44:07 +0800

    赶快回家

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -9,15 +9,15 @@ import java.util.Scanner;
 
 public class Start {
     public static void main(String[] args) {
-//        LdapUtil util = new LdapUtil();
-//        try {
-//            util.connectLdap();
-//            util.getWithAllChildrens(
-//                    new TopicEntry("all", "1",
-//                            "ou=all_test,dc=wsn,dc=com", null));
-//        } catch (NamingException e) {
-//            e.printStackTrace();
-//        }
+        LdapUtil util = new LdapUtil();
+        try {
+            util.connectLdap();
+            util.getWithAllChildrens(
+                    new TopicEntry("all", "1",
+                            "ou=all_test,dc=wsn,dc=com", null));
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
 
     }
 }
